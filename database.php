3a5beb94035723f4374722a34cf8cdcbf672846b
Paul Sizemore <paul.sizemore@gmail.com>
2019-06-20 16:21:47 -0400

    Update database.php

--- database.php
+++ database.php
@@ -3,9 +3,9 @@
 $servername = "localhost"; $username = "lovedjobs_crud"; $password = "L4jrD?n2Mbz~'UV4"; $dbname = "lovedjobs"; 
 
 define('DB_SERVER', "localhost");
-define('DB_USER', "lovedjobs_crud");
-define('DB_PASSWORD', "L4jrD?n2Mbz~'UV4");
-define('DB_DATABASE', "lovedjobs");
+define('DB_USER', "");
+define('DB_PASSWORD', "");
+define('DB_DATABASE', "");
 define('DB_DRIVER', "mysql");
 
 // We will use PDO to execute database stuff. 
